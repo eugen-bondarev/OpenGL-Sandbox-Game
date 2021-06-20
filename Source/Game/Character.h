a89eeeff8eb5eadf8be20d414c28a383bfd50fa8
azaremde <82723220+e-bondarev@users.noreply.github.com>
2021-06-20 16:17:45 +0200

    Working physics, needs refactoring

--- Source/Game/Character.h
+++ Source/Game/Character.h
@@ -74,7 +74,7 @@ public:
       velocity += -Physics::g * Time::delta * factor;
 
       Vec2 nextVelocity = velocity - Physics::g * Time::nextDelta * factor;
-      nextPosition = position + nextVelocity * Vec2(0, 1) * Time::nextDelta;
+      nextPosition = position + nextVelocity * 4.0f * Vec2(0, 1) * Time::nextDelta;
       nextModelMatrix = Math::Translate(Mat4(1), Vec3(nextPosition + Vec2(64, 0), 0.0));
 
       if (ceiling) {
@@ -94,7 +94,7 @@ public:
     Linow::AddLine(collider->GetStart(), collider->GetStart() + Vec2(1.0f, 0.0f) * 16.0f * 2.0f);
 
     for (int i = 2; i < 4; i++) {
-      Block block = GetBlock(position + Vec2(0, 0), Vec2(-5.0f, 5.0f), Vec2(i, 0));
+      Block block = GetBlock(position + Vec2(0, 0), Vec2(-5.0f, 5.0f), Vec2(i, -1));
       Block nextBlock = GetBlock(nextPosition + Vec2(0, 0), Vec2(-5.0f, 0.0f), Vec2(i, 0));
 
       Collider blockCollider({ 0.0f, 0.0f, 0.0f, 0.0f }, { 16, 16 });
@@ -104,24 +104,23 @@ public:
       Collider nextBlockCollider({ 0.0f, 0.0f, 0.0f, 0.0f }, { 16, 16 });
       nextBlockCollider.SetPosition(nextBlock.worldPosition);
 
-      // if (abs(velocity.y) > 100) {
-        if (blocks[nextBlock.index.x][nextBlock.index.y + 1] != BlockType::Empty) {    
-          for (int j = 1; j < 5; j++) {
-            if (blocks[nextBlock.index.x][nextBlock.index.y + 1 + j] == BlockType::Empty) {
-              onGround = true;
-              SetPositionY((nextBlock.index.y + j) * 16.0f + collider->GetRect().y.bottom);
-              // nextPosition = position;
-              break;
-            }
+      if (blocks[nextBlock.index.x][nextBlock.index.y + 1] != BlockType::Empty) {    
+        for (int j = 1; j < 5; j++) {
+          if (blocks[nextBlock.index.x][nextBlock.index.y + 1 + j] == BlockType::Empty) {
+            stopAt = (nextBlock.index.y + j) * 16.0f + collider->GetRect().y.bottom;
+            break;
           }
         }
-      // } else {
-      //   if (blocks[block.index.x][block.index.y + 1] != BlockType::Empty) {    
-      //     onGround = true;
-      //     SetPositionY((block.index.y) * 16.0f + collider->GetRect().y.bottom);
-      //     break;
-      //   }
-      // }
+      }
+
+      if (blocks[block.index.x][block.index.y + 1] != BlockType::Empty) {
+        if (stopAt) {
+          SetPositionY(stopAt);
+          stopAt = 0.0f;
+        }
+        onGround = true;
+        return;
+      }
     }
   }
 
