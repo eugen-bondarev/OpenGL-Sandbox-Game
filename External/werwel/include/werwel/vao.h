11f463df34c728882cc8c909f65e2fc3b1db5f76
e-bondarev <82723220+e-bondarev@users.noreply.github.com>
2021-07-04 22:17:48 +0200

    Werwel recovered

---
+++ External/werwel/include/werwel/vao.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "gpu_entity.h"
+#include "vertex_buffer_layout.h"
+#include "vbo.h"
+
+namespace Werwel {
+
+class VAO : public GPUEntity {
+public:
+	inline VAO() {
+		glGenVertexArrays(1, &handle);
+		WERWEL_DEBUG_LOG_OUT("[Call] Vao constructor");
+	}
+
+	inline ~VAO() override {		
+		Unbind();
+		glDeleteVertexArrays(1, &handle);
+
+		WERWEL_DEBUG_LOG_OUT("[Call] Vao destructor");
+	}
+
+	inline void Bind() const override {
+		glBindVertexArray(handle);
+
+		for (const auto& attribute : attributes) {
+			glEnableVertexAttribArray(attribute);
+		}
+	}
+
+	inline void Unbind() const override {
+		for (int i = attributes.size() - 1; i >= 0; --i) {
+			glDisableVertexAttribArray(attributes[i]);
+		}
+		
+		glBindVertexArray(0);
+	}
+
+	inline GLuint GetVertexCount() const {
+		return vertexCount;
+	}
+
+	inline void AddVbo(GLuint vbo) {
+		buffers.push_back(vbo);
+	}
+
+	inline void AddAttribute(GLuint attribute) {
+		attributes.push_back(attribute);
+	}
+
+	template <typename... Args>
+	inline Mem::Ref<VBO> AddVBO(Args... args) {
+		Mem::Ref<VBO> vbo = Mem::CreateRef<VBO>(GetLastAttribute(), std::forward<Args>(args)...);
+
+		auto usedAttributes = vbo->GetUsedAttributes();
+		for (int i = 0; i < usedAttributes.size(); i++) {
+			attributes.push_back(usedAttributes[i]);
+		}
+
+		vbos.push_back(vbo);
+
+		if (vbo->GetType() == GL_ELEMENT_ARRAY_BUFFER) {
+			indexBuffer = vbo;
+			vertexCount = vbo->GetIndexCount();
+		}
+
+		return vbo;
+	}
+
+	inline GLuint GetLastAttribute() const {
+		return static_cast<GLuint>(attributes.size());
+	}
+
+	inline const Mem::Ref<VBO>& GetIndexBuffer() const {
+		return indexBuffer;
+	}
+
+private:
+	std::vector<Mem::Ref<VBO>> vbos;
+	Mem::Ref<VBO> indexBuffer;
+
+	GLuint vertexCount { 0 };
+	
+	std::vector<GLuint> attributes;
+	std::vector<GLuint> buffers;
+
+	VAO(const VAO &) = delete;
+	VAO &operator=(const VAO &) = delete;
+};
+
+}
---
+++ External/werwel/src/shader.cpp
@@ -0,0 +1,120 @@
+#include "Shader.h"
+
+namespace Werwel {
+
+Shader::~Shader() {
+	Unbind();
+	
+	glDetachShader(handle, vsHandle);
+	glDetachShader(handle, fsHandle);
+	glDeleteShader(vsHandle);
+	glDeleteShader(fsHandle);
+	glDeleteProgram(handle);
+
+	WERWEL_DEBUG_LOG_OUT("[Call] Shader destructor");
+}
+
+void Shader::Link() const {
+	glLinkProgram(handle);
+
+	if (vsHandle != 0) {
+		glDetachShader(handle, vsHandle);
+	}
+
+	if (fsHandle != 0) {
+		glDetachShader(handle, fsHandle);
+	}
+
+	glValidateProgram(handle);
+}
+
+GLuint Shader::CreateShader(const std::string& shaderCode, GLuint shaderType) {
+	const unsigned int shaderID = glCreateShader(shaderType);
+
+	const char* c_str = shaderCode.c_str();
+
+	glShaderSource(shaderID, 1, &c_str, NULL);
+	glCompileShader(shaderID);
+
+	int status;
+	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &status);
+
+	if (!status) {
+		int length;
+		glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &length);
+		Str log;
+		log.resize(length);
+		glGetShaderInfoLog(shaderID, length, &length, &log[0]);
+
+		Str errorFunctionName = "--------[ " "Foo" " ]--------";
+		Str separator; for (size_t i = 0; i < errorFunctionName.size(); i++) separator += "-";
+
+		WERWEL_LOG_OUT(errorFunctionName);
+		WERWEL_LOG_OUT("Error occured while compiling a shader: " << log);
+		WERWEL_LOG_OUT(separator);
+
+		return -1;
+	}
+
+	glAttachShader(handle, shaderID);
+
+	return shaderID;
+}
+
+void Shader::Bind() const {
+	glUseProgram(handle);
+}
+
+void Shader::Unbind() const {
+	glUseProgram(0);
+}
+
+void Shader::CreateUniform(const std::string& name) {
+	int location = glGetUniformLocation(handle, name.c_str());
+
+	uniformLocations.insert(
+		std::pair<std::string, int>(
+			name.c_str(),
+			location
+			)
+	);
+}
+
+void Shader::SetFloat(const std::string& name, GLfloat value) {
+	glUniform1f(uniformLocations.at(name), value);
+}
+
+void Shader::SetInt(const std::string& name, GLint value) {
+	glUniform1i(uniformLocations.at(name), value);
+}
+
+void Shader::SetMat2x4(const std::string& name, float const* const matrix) {
+	glUniformMatrix2x4fv(uniformLocations.at(name), 1, GL_FALSE, matrix);
+}
+
+void Shader::SetMat4x4(const std::string& name, float const* const matrix) {
+	glUniformMatrix4fv(uniformLocations.at(name), 1, GL_FALSE, matrix);
+}
+
+
+void Shader::SetMat3x3(const std::string& name, float const* const matrix) {
+	glUniformMatrix3fv(uniformLocations.at(name), 1, GL_FALSE, matrix);
+}
+
+void Shader::SetVec2(const std::string& name, float const* const vec) {
+	glUniform2fv(uniformLocations.at(name), 1, vec);
+}
+
+void Shader::SetVec3(const std::string& name, float const* const vec) {
+	glUniform3fv(uniformLocations.at(name), 1, vec);
+}
+
+void Shader::SetVec4(const std::string& name, float const* const vec) {
+	glUniform4fv(uniformLocations.at(name), 1, vec);
+}
+
+void Shader::SetListMat4x4(const std::string& name, float const* const list, unsigned int size) {
+	glUniformMatrix4fv(uniformLocations.at(name), size, GL_FALSE, list);
+}
+
+}
